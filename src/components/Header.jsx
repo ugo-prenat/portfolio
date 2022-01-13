4300892dbf539c2ab9887590e050ae075355bf58
Ugo <ugo.prenat@gmail.com>
2022-01-13 10:44:53 +0100

    Dev version indication

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Lottie from 'react-lottie'
 
 import { HashLink as Link } from 'react-router-hash-link'
@@ -9,14 +9,22 @@ import css from '../styles/header.module.css'
 import downloadAnim from '../assets/animations/download.json'
 
 export default function Header() {
+  const [isProdVersion, setIsProdVersion] = useState(false)
   const [sequence, setSequence] = useState({
     segments: [0, 1],
     forceFlag: true
   })
+  
+  useEffect(() => {
+    // Check if the user is on the dev version of the website
+    const appDomain = 'localhost'
+    const hostname = window.location.hostname
+    if (hostname === `dev.${appDomain}`) setIsProdVersion(true)
+  }, [])
 
   const animOptions = {
     loop: false,
-    autoplay: false,
+    autoplay: true,
     animationData: downloadAnim,
     rendererSettings: {
       preserveAspectRatio: "xMidYMid slice"
@@ -25,6 +33,9 @@ export default function Header() {
 
     return (
       <div className={css.container}>
+        <div className={css.devVersion}>
+          { isProdVersion && <p>DEV</p> }
+        </div>
 
         <div className={css.links}>
           <Link to='/#projets'>Projets</Link>
