0ef6804ba9dbed561653991224a5a198ba34dfed
Ugo <ugo.prenat@gmail.com>
2022-01-14 20:25:49 +0100

    continue archive page

--- src/components/archivePage/ProjectList.jsx
+++ src/components/archivePage/ProjectList.jsx
@@ -9,9 +9,46 @@ export default function ProjectsList() {
 
   return (
     <div className={css.projectList}>
-      { projects.map((project, index) => {
-        return ( <ProjectCard data={project} key={index} /> )
-      }).reverse() }
+      {/* <table>
+        <thead>
+          <tr>
+            <th>Sortie</th>
+            <th>Titre</th>
+            <th>Technologies</th>
+            <th>Lien</th>
+          </tr>
+        </thead>
+        
+        <tbody>
+          { projects.map((project, index) => {
+            return (
+              <tr className={css.project}>
+                <td>{project.launchDate}</td>
+                <td>{project.title}</td>
+                <td>{project.technologies[0]}</td>
+                <td>{project.githubLink}</td>
+              </tr>
+            )
+          }).reverse() }
+        </tbody>
+      </table> */}
+      
+      <table>
+        <thead>
+          <tr>
+            <th>Sortie</th>
+            <th>Titre</th>
+            <th>Technologies</th>
+            <th>Lien</th>
+          </tr>
+        </thead>
+        
+        <tbody>
+          { projects.map((project, index) => {
+            return ( <ProjectCard data={project} key={index} /> )
+          }).reverse()}
+        </tbody>
+      </table>
     </div>
   )
 }
--- src/components/pages/Archive.jsx
+++ src/components/pages/Archive.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from 'react'
 
 import ProjectsList from '../archivePage/ProjectList'
+import Contact from '../contact/Contact'
 
 import css from '../../styles/archive.module.css'
 
@@ -11,7 +12,14 @@ export default function Archive() {
   
   return (
     <div className={css.component}>
+      <div className={css.title}>
+        <h2>Archives</h2>
+        <p>La liste des projets sur lesquelles j'ai travaillés</p>
+      </div>
+      
       <ProjectsList />
+      
+      <Contact />
     </div>
   )
 }
