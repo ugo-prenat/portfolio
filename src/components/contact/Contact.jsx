d22f20ab2b8d0584c2d5cfab25d5637582ed3529
Ugo <ugo.prenat@gmail.com>
2022-01-27 16:20:48 +0100

    rebuild experiences component

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -33,7 +33,7 @@ export default function Contact() {
       <div className={css.footer}>
         <p>Designé et développé par mes soins</p>
         <div>
-          <a href="https://github.com/ugo-prenat/portfolio" target='_blank' rel='noreferrer'>Le code du projet</a>
+          <a href="https://github.com/ugo-prenat/portfolio" target='_blank' rel='noreferrer'>Repo GitHub du portfolio</a>
           <svg xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink" aria-hidden="true" role="img" preserveAspectRatio="xMidYMid meet" viewBox="0 0 24 24"><g fill="currentColor"><path d="M5 6a1 1 0 0 1 1-1h4a1 1 0 1 0 0-2H6a3 3 0 0 0-3 3v12a3 3 0 0 0 3 3h12a3 3 0 0 0 3-3v-4a1 1 0 1 0-2 0v4a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1V6zm10-3a1 1 0 1 0 0 2h2.586l-6.293 6.293a1 1 0 0 0 1.414 1.414L19 6.414V9a1 1 0 1 0 2 0V4a1 1 0 0 0-1-1h-5z"/></g></svg>
         </div>
       </div>
--- src/components/experiences/Experiences.jsx
+++ src/components/experiences/Experiences.jsx
@@ -8,14 +8,15 @@ import css from '../../styles/experiences.module.css'
 export default function Experiences() {
   return (
     <div className={`component ${css.component}`} id='experiences'>
-      
-      <SectionTitle color='black'>Expériences</SectionTitle>
-      
-      <p className={css.sectionDescription}>
-        Mon formation à la Coding Factory m’a permit de réaliser un stage de deux mois ainsi qu’une année en alternance.
-        <br/>
-        Je cherche actuellement une entreprise où je pourrai réaliser ma dernière année de licence.
-      </p>
+      <div className={css.experienceHeader}>
+        <SectionTitle>Expériences</SectionTitle>
+        
+        <p className={css.sectionDescription}>
+          Ma formation à la Coding Factory m’a permis de réaliser un stage de deux mois ainsi qu’une année en alternance.
+          <br/>
+          Je cherche actuellement une entreprise où je pourrai réaliser ma dernière année de licence.
+        </p>
+      </div>
       
       <ExperienceList />
       
