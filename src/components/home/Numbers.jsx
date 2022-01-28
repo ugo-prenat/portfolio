4e9de79334176f225658876838c0bc6210e0b3d6
Ugo <ugo.prenat@gmail.com>
2022-01-28 19:05:24 +0100

    favicon + numbers's animation

--- src/components/home/Numbers.jsx
+++ src/components/home/Numbers.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import Aos from 'aos';
 
 import css from '../../styles/home.module.css'
 
@@ -8,9 +9,10 @@ export default function Numbers(props) {
   const lineHeight = 35
   
   useEffect(() => {
+    Aos.init({ duration: 400 })
     const tempNumbers = []
     
-    setTimeout(() => {
+    /* setTimeout(() => {
       setContainerHeight(props.data.current.clientHeight)
       
       const nbLines = Math.floor(containerHeight / lineHeight)
@@ -19,15 +21,33 @@ export default function Numbers(props) {
         tempNumbers.push(i)
       }
       setNumbers(tempNumbers)
-    }, 500)
+    }, 500) */
+    
+    setContainerHeight(props.data.current.clientHeight)
+    const nbLines = Math.floor(containerHeight / lineHeight)
+    
+    for (let i = 1; i <= nbLines; i++) {
+      tempNumbers.push(i)
+    }
+    setNumbers(tempNumbers)
+    
   }, [props.data, containerHeight])
     
   
   return (
     <div className={css.numbersContainer}>
       <div className={css.numbers}>
-        { numbers.map(number => {
-          return ( <p key={number}>{number}</p> )
+        { numbers.map((number, index) => {
+          return (
+            <p
+              key={number}
+              data-aos='fade-right'
+              data-aos-delay={`${50 * index}`}
+              data-aos-once={true}
+            >
+              {number}
+            </p>
+          )
         }) }
       </div>
     </div>
